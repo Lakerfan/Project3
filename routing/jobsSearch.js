347d2dd8db99e4da3e9ae4ff8d21182ea8cfa2f1
Griffin Sprague <griffin.spraguedixon23@gmail.com>
2019-11-09 12:04:09 -0800

    Fixed the small bug in the jobSearch Code

--- routing/jobsSearch.js
+++ routing/jobsSearch.js
@@ -1,13 +1,13 @@
 const axios = require('axios');
 const router = require('express').Router();
 
-axios.get('/', (req, res) => {
+router.get('/', (req, res) => {
     axios
         .get(
             `https://api.adzuna.com/v1/api/jobs/us/search/1?app_id=${process.env.REACT.APP.PROJECT_3_APP_ID}&app_key=${process.env.REACT.APP.PROJECT_3_APP_KEY}`,
-            ({params: req.query})
-        .then(({data: {results} }) => results.json(results))
-        .catch(err => res.status(422).json(err); 
+            {params: req.query})
+        .then(({ data: {results} }) => res.json(results))
+        .catch(err => res.status(422).json.err)
 })
 
 module.exports = router;

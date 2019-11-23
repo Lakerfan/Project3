6b7b0dc4756b97bec8283dd8bfbc7acf2891bf52
Griffin Sprague <griffin.spraguedixon23@gmail.com>
2019-11-22 21:32:56 -0800

    took out routes

--- routes/api/jobs.js
+++ routes/api/jobs.js
@@ -5,10 +5,4 @@ router.route('/')
     .get(jobsController.findAll)
     .post(jobsController.create);
 
-router
-    .route('/:id')
-    .get(jobsController.findById)
-    .put(jobsController.update)
-    .delete(jobsController.remove);
-
 module.exports = router;
